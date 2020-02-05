b0b81e43070e9b4669cbbffeee5ddd1aac5aaac5
Calvin Xu <calvinxu806@gmail.com>
2020-02-04 17:14:22 -0800

    Updated lift constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/lift/LiftConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/lift/LiftConstants.java
@@ -16,15 +16,33 @@ public class LiftConstants {
     public static final double MAX_ACC = 0;
     public static final double MAX_JERK = 0;
 
+    public static double kV = 1.0 / rpmToVelocity(getMaxRpm());
+    public static double kA = 0;
+    public static double kStatic = 0;
+
     public static double SPOOL_RADIUS = 0.311;
     public static double GEAR_RATIO = 42.0 / 48.0; // output (wheel) speed / input (motor) speed
 
-    public MotionProfile generateLiftMotionProfile(MotionState start, MotionState end) {
+    public static MotionProfile generateLiftMotionProfile(MotionState start, MotionState end) {
         return MotionProfileGenerator.generateSimpleMotionProfile(start, end, MAX_VEL, MAX_ACC, MAX_JERK, false);
     }
 
     public static double encoderTicksToInches(double ticks) {
         return SPOOL_RADIUS * 2 * Math.PI * GEAR_RATIO * ticks / 293;
         // 293: calculated & experimentally confirmed
+    }
+
+    public static double rpmToVelocity(double rpm) {
+        return rpm * GEAR_RATIO * 2 * Math.PI * SPOOL_RADIUS / 60.0;
+    }
+
+    public static double getMaxRpm() {
+//        return MOTOR_CONFIG.getMaxRPM() *
+//                (RUN_USING_ENCODER ? MOTOR_CONFIG.getAchieveableMaxRPMFraction() : 1.0);
+        // internal gear ratio: 304/29
+        // 6000: http://www.revrobotics.com/rev-41-1301/
+        // default AchieveableMaxRPMFraction: 0.85
+//        return 6000.0 / (304.0 / 29.0) * 0.85;
+        return 486;
     }
 }
